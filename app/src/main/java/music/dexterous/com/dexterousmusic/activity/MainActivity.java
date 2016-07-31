9c8f9f700a40349f2d00e3a8f93171cdc489f35b
Dubey's <abhishekdubey331@gmail.com>
2016-07-31 21:37:38 +0530

    Pushed DB Helper Classes

--- app/src/main/java/music/dexterous/com/dexterousmusic/activity/MainActivity.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package music.dexterous.com.dexterousmusic.activity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,11 +11,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import music.dexterous.com.dexterousmusic.R;
 import music.dexterous.com.dexterousmusic.customeviews.ShortToast;
 import music.dexterous.com.dexterousmusic.misc.PrettyLogger;
+import music.dexterous.com.dexterousmusic.services.ScanMusicService;
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.OnNeverAskAgain;
 import permissions.dispatcher.OnPermissionDenied;
@@ -24,6 +27,7 @@ import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+    protected Button scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        scan = (Button) findViewById(R.id.scan_music);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showStoragePermission();
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -47,10 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
     void showStoragePermission() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.sample_content_fragment, StoragePreviewFragment.newInstance())
-                .addToBackStack("storage")
-                .commitAllowingStateLoss();
+        Intent intent = new Intent(getApplicationContext(), ScanMusicService.class);
+        startService(intent);
+
     }
 
     @OnShowRationale(Manifest.permission.READ_EXTERNAL_STORAGE)
--- app/src/main/java/music/dexterous/com/dexterousmusic/utils/music/ScanningMusic.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/utils/music/ScanningMusic.java
@@ -8,9 +8,11 @@ import android.os.Parcelable;
 import android.provider.MediaStore;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import music.dexterous.com.dexterousmusic.database.MusicLibraryTable;
+import music.dexterous.com.dexterousmusic.misc.PrettyLogger;
 
 /**
  * Created by Dubey's on 31-07-2016.
@@ -103,6 +105,8 @@ public class ScanningMusic {
             }
             while (cursor.moveToNext());
         }
+        PrettyLogger.e(Arrays.toString(musicLibraryTables.toArray()));
+        PrettyLogger.e("" + musicLibraryTables.size());
 
 
         scanFromExternal("external", c);
@@ -120,6 +124,51 @@ public class ScanningMusic {
         Uri playlistUri = ((fromWhere == "internal") ?
                 android.provider.MediaStore.Audio.Playlists.INTERNAL_CONTENT_URI :
                 android.provider.MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI);
+        ContentResolver resolver = c.getContentResolver();
+        Cursor cursor;
+        final String musicsOnly = MediaStore.Audio.Media.IS_MUSIC + "=1";
+
+
+        String[] columns = {
+                SONG_ID,
+                SONG_TITLE,
+                SONG_ARTIST,
+                SONG_ALBUM,
+                SONG_YEAR,
+                SONG_TRACK_NO,
+                SONG_FILEPATH,
+                SONG_DURATION
+        };
+        cursor = resolver.query(musicUri, columns, musicsOnly, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            // NOTE: I tried to use MediaMetadataRetriever, but it was too slow.
+            //       Even with 10 songs, it took like 13 seconds,
+            //       No way I'm releasing it this way - I have like 4.260 songs!
+
+            do {
+                // Creating a song from the values on the row
+                MusicLibraryTable song = new MusicLibraryTable();
+
+
+                song.setSONG_ID("" + cursor.getInt(cursor.getColumnIndex(SONG_ID)));
+                song.setSONG_FILE_PATH(cursor.getString(cursor.getColumnIndex(SONG_FILEPATH)));
+                song.setSONG_TITLE(cursor.getString(cursor.getColumnIndex(SONG_TITLE)));
+                song.setSONG_ARTIST(cursor.getString(cursor.getColumnIndex(SONG_ARTIST)));
+                song.setSONG_ALBUM(cursor.getString(cursor.getColumnIndex(SONG_ALBUM)));
+                song.setSONG_YEAR("" + cursor.getInt(cursor.getColumnIndex(SONG_YEAR)));
+                song.setSONG_TRACK_NUMBER("" + cursor.getInt(cursor.getColumnIndex(SONG_TRACK_NO)));
+                song.setSONG_DURATION("" + cursor.getInt(cursor.getColumnIndex(SONG_DURATION)));
+
+                musicLibraryTables.add(song);
+            }
+            while (cursor.moveToNext());
+        }
+        PrettyLogger.e(Arrays.toString(musicLibraryTables.toArray()));
+        PrettyLogger.e("" + musicLibraryTables.size());
+
+
+
 
     }
 
