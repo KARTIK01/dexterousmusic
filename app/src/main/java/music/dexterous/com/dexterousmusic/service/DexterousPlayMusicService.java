99a32122445b0fae9e69fc808701d4bb2e0bc5da
kartik_agarwal01 <kartik@zuppit.in>
2016-07-31 23:45:20 +0530

    music serverce added

--- app/src/main/java/music/dexterous/com/dexterousmusic/service/DexterousPlayMusicService.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/service/DexterousPlayMusicService.java
@@ -2,6 +2,8 @@ package music.dexterous.com.dexterousmusic.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.os.PowerManager;
 
 import music.dexterous.com.dexterousmusic.musicutils.DexterousMediaPlayer;
 
@@ -18,6 +20,18 @@ public class DexterousPlayMusicService extends IntentService {
      */
     DexterousMediaPlayer dexterousMediaPlayer;
 
+    /**
+     * AudioManager provides access to volume and ringer mode control.
+     * <p>
+     * Use this to get audio focus:
+     * <p>
+     * 1. Making sure other music apps don't play
+     * at the same time;
+     * 2. Guaranteeing the lock screen widget will
+     * be controlled by us;
+     */
+    AudioManager audioManager;
+
 
     public DexterousPlayMusicService() {
         super("DexterousPlayMusicService");
@@ -26,8 +40,42 @@ public class DexterousPlayMusicService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
+            audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            initMusicPlayer();
 
         }
     }
 
+    /**
+     * Initializes the Android's internal MediaPlayer.
+     *
+     * @note We might call this function several times without
+     * necessarily calling {@link #stopMusicPlayer()}.
+     */
+    public void initMusicPlayer() {
+        if (dexterousMediaPlayer == null) {
+            dexterousMediaPlayer = new DexterousMediaPlayer();
+        }
+
+        // Assures the CPU continues running this service
+        // even when the device is sleeping.
+        dexterousMediaPlayer.setWakeMode(this,
+                PowerManager.PARTIAL_WAKE_LOCK);
+
+        dexterousMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        // These are the events that will "wake us up"
+        dexterousMediaPlayer.setOnPreparedListener(this); // player initialized
+        dexterousMediaPlayer.setOnCompletionListener(this); // song completed
+        dexterousMediaPlayer.setOnErrorListener(this);
+    }
+
+
+    public void stopMusicPlayer() {
+        if (dexterousMediaPlayer == null)
+            return;
+
+        dexterousMediaPlayer.stop();
+        dexterousMediaPlayer.release();
+    }
 }
