525fbd83dd1c6ad2461297fa4f8ca9a3b2cb5234
kartik_agarwal01 <kartik@zuppit.in>
2016-08-07 15:15:35 +0530

    before starting ui

---
+++ app/src/main/java/music/dexterous/com/dexterousmusic/notification/Helper.java
@@ -0,0 +1,47 @@
+package music.dexterous.com.dexterousmusic.notification;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import music.dexterous.com.dexterousmusic.R;
+import music.dexterous.com.dexterousmusic.database.Music;
+import music.dexterous.com.dexterousmusic.receiver.widget.NextMusicReceiver;
+import music.dexterous.com.dexterousmusic.receiver.widget.PlayMusicReceiver;
+
+/**
+ * Created by Honey on 8/7/2016.
+ */
+public class Helper {
+
+    // Manually settings the buttons and text
+    // (ignoring the defaults on the XML)
+    static protected void setSmallNotificationView(RemoteViews smallNotificationView, Music music) {
+        smallNotificationView.setImageViewResource(R.id.notification_button_play, R.drawable.ic_pause);
+        smallNotificationView.setImageViewResource(R.id.notification_button_skip, R.drawable.ic_skip);
+        smallNotificationView.setTextViewText(R.id.notification_text_title, music.getSONG_TITLE());
+        smallNotificationView.setTextViewText(R.id.notification_text_artist, music.getSONG_ARTIST());
+    }
+
+    // On the notification we have two buttons - Play and Skip
+    // Here we make sure the class `NotificationButtonHandler`
+    // gets called when user selects one of those.
+    //
+    // First, building the play button and attaching it.
+    static protected void setButtonPlayIntent(Context context, RemoteViews smallNotificationView) {
+        Intent buttonPlayIntent = new Intent(context, PlayMusicReceiver.class);
+        buttonPlayIntent.putExtra(PlayMusicReceiver.ACTION, PlayMusicReceiver.ACTION_TYPE_TOGGLE);
+        PendingIntent buttonPlayPendingIntent = PendingIntent.getBroadcast(context, 0, buttonPlayIntent, 0);
+        smallNotificationView.setOnClickPendingIntent(R.id.notification_button_play, buttonPlayPendingIntent);
+    }
+
+    // And now, building and attaching the Skip button.
+    static protected void setButtonSkipIntent(Context context, RemoteViews smallNotificationView) {
+        Intent buttonSkipIntent = new Intent(context, NextMusicReceiver.class);
+        buttonSkipIntent.putExtra(NextMusicReceiver.ACTION, NextMusicReceiver.ACTION_TYPE_SKIP);
+        PendingIntent buttonSkipPendingIntent = PendingIntent.getBroadcast(context, 0, buttonSkipIntent, 0);
+        smallNotificationView.setOnClickPendingIntent(R.id.notification_button_skip, buttonSkipPendingIntent);
+    }
+
+}
--- app/src/main/java/music/dexterous/com/dexterousmusic/notification/NotificationMusic.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/notification/NotificationMusic.java
@@ -1,19 +1,27 @@
 package music.dexterous.com.dexterousmusic.notification;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
 import android.widget.RemoteViews;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 import music.dexterous.com.dexterousmusic.R;
 import music.dexterous.com.dexterousmusic.activity.NowPlayingActivity;
 import music.dexterous.com.dexterousmusic.database.Music;
-import music.dexterous.com.dexterousmusic.receiver.widget.NextMusicReceiver;
-import music.dexterous.com.dexterousmusic.receiver.widget.PlayMusicReceiver;
 import music.dexterous.com.dexterousmusic.utils.android.Package;
 
 /**
@@ -36,12 +44,12 @@ public class NotificationMusic extends NotificationSimple {
     /**
      * Used to create and update the same notification.
      */
-    Notification.Builder notificationBuilder = null;
+    NotificationCompat.Builder notificationBuilder = null;
 
     /**
      * Custom appearance of the notification, also updated.
      */
-    RemoteViews notificationView = null;
+    RemoteViews smallNotificationView = null;
 
     /**
      * Used to actually broadcast the notification.
@@ -86,6 +94,7 @@ public class NotificationMusic extends NotificationSimple {
         Intent notifyIntent = new Intent(context, NowPlayingActivity.class);
         notifyIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        //TODO build TaskStackBuilder for notification
         // Letting the Intent be executed later by other application.
         PendingIntent pendingIntent = PendingIntent.getActivity
                 (context,
@@ -95,45 +104,40 @@ public class NotificationMusic extends NotificationSimple {
 
 
         // Setting our custom appearance for the notification
-        notificationView = new RemoteViews(Package.getPackageName(service), R.layout.notification_big);
+        smallNotificationView = new RemoteViews(Package.getPackageName(service), R.layout.notification_small);
 
-        // Manually settings the buttons and text
-        // (ignoring the defaults on the XML)
-        notificationView.setImageViewResource(R.id.notification_button_play, R.drawable.ic_pause);
-        notificationView.setImageViewResource(R.id.notification_button_skip, R.drawable.ic_skip);
-        notificationView.setTextViewText(R.id.notification_text_title, music.getSONG_TITLE());
-        notificationView.setTextViewText(R.id.notification_text_artist, music.getSONG_ARTIST());
-
-        // On the notification we have two buttons - Play and Skip
-        // Here we make sure the class `NotificationButtonHandler`
-        // gets called when user selects one of those.
-        //
-        // First, building the play button and attaching it.
-        Intent buttonPlayIntent = new Intent(context, PlayMusicReceiver.class);
-        buttonPlayIntent.putExtra(PlayMusicReceiver.ACTION, PlayMusicReceiver.ACTION_TYPE_TOGGLE);
-        PendingIntent buttonPlayPendingIntent = PendingIntent.getBroadcast(context, 0, buttonPlayIntent, 0);
-        notificationView.setOnClickPendingIntent(R.id.notification_button_play, buttonPlayPendingIntent);
-
-        // And now, building and attaching the Skip button.
-        Intent buttonSkipIntent = new Intent(context, NextMusicReceiver.class);
-        buttonSkipIntent.putExtra(NextMusicReceiver.ACTION, NextMusicReceiver.ACTION_TYPE_SKIP);
-        PendingIntent buttonSkipPendingIntent = PendingIntent.getBroadcast(context, 0, buttonSkipIntent, 0);
-        notificationView.setOnClickPendingIntent(R.id.notification_button_skip, buttonSkipPendingIntent);
+        //set resources to view from small notification
+        Helper.setSmallNotificationView(smallNotificationView, music);
+        Helper.setButtonPlayIntent(context, smallNotificationView);
+        Helper.setButtonSkipIntent(context, smallNotificationView);
 
         // Finally... Actually creating the Notification
-        notificationBuilder = new Notification.Builder(context);
+        notificationBuilder = new NotificationCompat.Builder(context);
 
         notificationBuilder.setContentIntent(pendingIntent)
                 .setSmallIcon(getSmallIcon())
                 //TODO
-                .setTicker("kMP: Playing '" + music.getSONG_TITLE() + "' from '" + music.getSONG_ARTIST() + "'")
+                .setTicker("Playing '" + music.getSONG_TITLE() + "' from '" + music.getSONG_ARTIST() + "'")
                 .setOngoing(true)
                 .setContentTitle(music.getSONG_TITLE())
                 .setContentText(music.getSONG_ARTIST())
-                .setContent(notificationView);
+                .setContent(smallNotificationView);
 
         Notification notification = notificationBuilder.build();
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            RemoteViews bigContentView = new RemoteViews(Package.getPackageName(context), R.layout.notification_big);
+//            bigContentView.setTextViewText(R.id.bigNotificationTitle, title);
+
+            notification.bigContentView = bigContentView;
+            notification.priority = Notification.PRIORITY_MAX;
+        }
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            notification.visibility = Notification.VISIBILITY_PUBLIC;
+        }
 
         notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 //		notificationManager.notify(NOTIFICATION_ID, notification);
@@ -148,16 +152,16 @@ public class NotificationMusic extends NotificationSimple {
      * Updates the Notification icon if the music is paused.
      */
     public void notifyPaused(boolean isPaused) {
-        if ((notificationView == null) || (notificationBuilder == null))
+        if ((smallNotificationView == null) || (notificationBuilder == null))
             return;
 
         int iconID = ((isPaused) ?
                 R.drawable.ic_play :
                 R.drawable.ic_pause);
 
-        notificationView.setImageViewResource(R.id.notification_button_play, iconID);
+        smallNotificationView.setImageViewResource(R.id.notification_button_play, iconID);
 
-        notificationBuilder.setContent(notificationView);
+        notificationBuilder.setContent(smallNotificationView);
 
 //		notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
 
