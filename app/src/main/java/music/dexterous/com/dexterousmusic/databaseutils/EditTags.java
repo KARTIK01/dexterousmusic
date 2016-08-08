8a3103d7c7e6062e2774fd0e60d2e3190c11c672
kartik_agarwal01 <kartik@zuppit.in>
2016-08-08 22:51:39 +0530

    bb

---
+++ app/src/main/java/music/dexterous/com/dexterousmusic/databaseutils/EditTags.java
@@ -0,0 +1,8 @@
+package music.dexterous.com.dexterousmusic.databaseutils;
+
+/**
+ * Created by Honey on 8/8/2016.
+ */
+
+public class EditTags {
+}
---
+++ app/src/main/java/music/dexterous/com/dexterousmusic/utils/ExampleLyrics.java
@@ -0,0 +1,8 @@
+package music.dexterous.com.dexterousmusic.utils;
+
+/**
+ * Created by Honey on 8/7/2016.
+ */
+
+public class ExampleLyrics {
+}
