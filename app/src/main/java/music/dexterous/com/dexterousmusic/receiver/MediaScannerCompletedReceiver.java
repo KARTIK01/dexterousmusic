994db8f3a5fa4c627f19939f42ee3f11ab4ef8e7
kartik_agarwal01 <kartik@zuppit.in>
2016-08-10 22:22:18 +0530

    changes

--- app/src/main/java/music/dexterous/com/dexterousmusic/receiver/MediaScannerCompletedReceiver.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/receiver/MediaScannerCompletedReceiver.java
@@ -3,12 +3,12 @@ package music.dexterous.com.dexterousmusic.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Vibrator;
 
 import music.dexterous.com.dexterousmusic.service.ScanMusicService;
 import music.dexterous.com.dexterousmusic.utils.preference.AppPreference;
 
-//TODO check this is not working
 public class MediaScannerCompletedReceiver extends BroadcastReceiver {
 
     public MediaScannerCompletedReceiver() {
@@ -19,11 +19,12 @@ public class MediaScannerCompletedReceiver extends BroadcastReceiver {
         if (intent != null) {
             switch (intent.getAction()) {
                 case Intent.ACTION_MEDIA_SCANNER_FINISHED:
-                    Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-                    v.vibrate(new long[]{0, 2000, 1000}, 0);
-                    AppPreference.putString("ACTION_MEDIA_SCANNER_FINISHED", intent.getData().toString());
+                    Uri newMediaInsertedUri = intent.getData();
 
                     //TODO check if music is adedd in app then show notificaton for more songs added in app
+                    // it can be done in 2 ways
+                    //1 . check uri returned in abpve url
+                    //2. check out database state then show notification
 
 
                     //TODO check if we has permission for access
