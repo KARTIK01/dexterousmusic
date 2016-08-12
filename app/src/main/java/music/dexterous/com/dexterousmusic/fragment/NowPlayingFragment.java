f01ec003dc86b116ee4352b15a276f8d8be75518
kartik_agarwal01 <kartik@zuppit.in>
2016-08-13 00:46:58 +0530

    waves added

--- app/src/main/java/music/dexterous/com/dexterousmusic/fragment/NowPlayingFragment.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/fragment/NowPlayingFragment.java
@@ -6,11 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cleveroad.audiovisualization.AudioVisualization;
+import com.cleveroad.audiovisualization.DbmHandler;
+import com.cleveroad.audiovisualization.SpeechRecognizerDbmHandler;
+import com.cleveroad.audiovisualization.VisualizerDbmHandler;
+
 import music.dexterous.com.dexterousmusic.R;
 import music.dexterous.com.dexterousmusic.activity.BaseActivity;
 
 public class NowPlayingFragment extends BaseFragment {
 
+    private AudioVisualization audioVisualization;
 
     public static NowPlayingFragment newInstance() {
         NowPlayingFragment fragment = new NowPlayingFragment();
@@ -28,5 +34,37 @@ public class NowPlayingFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        audioVisualization = (AudioVisualization) view.findViewById(R.id.visualizer_view);
+
+
+        audioVisualization.linkTo(DbmHandler.Factory.newVisualizerHandler(getContext(), 0));
+
+        // set speech recognizer handler
+//        SpeechRecognizerDbmHandler speechRecHandler = DbmHandler.Factory.newSpeechRecognizerHandler(getActivity());
+//        speechRecHandler.innerRecognitionListener(...);
+//        audioVisualization.linkTo(speechRecHandler);
+//
+//        // set audio visualization handler. This will REPLACE previously set speech recognizer handler
+//        VisualizerDbmHandler vizualizerHandler = DbmHandler.Factory.newVisualizerHandler(getContext(), 0);
+//        audioVisualization.linkTo(vizualizerHandler);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        audioVisualization.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        audioVisualization.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        audioVisualization.release();
+        super.onDestroyView();
     }
 }
