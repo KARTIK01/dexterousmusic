1b99a50a5f8330682c4c273b22e6a91497f2e82d
Dubey's <abhishekdubey331@gmail.com>
2016-08-25 21:03:26 +0530

    BroadcastReceiver changes

--- app/src/main/java/music/dexterous/com/dexterousmusic/receiver/HeadsetBroadcastReceiver.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/receiver/HeadsetBroadcastReceiver.java
@@ -3,23 +3,37 @@ package music.dexterous.com.dexterousmusic.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import music.dexterous.com.dexterousmusic.utils.logger.PrettyLogger;
-import music.dexterous.com.dexterousmusic.utils.preference.UsersAppPreference;
+import android.util.Log;
+import android.widget.Toast;
 
 //TODO
 public class HeadsetBroadcastReceiver extends BroadcastReceiver {
+    String TAG = "Receiver";
+
     public HeadsetBroadcastReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent != null && intent.getAction().equals(Intent.ACTION_HEADSET_PLUG) && UsersAppPreference.getMusicPlayONInsetHeadSet()) {
-            PrettyLogger.d("HeadPhone Inserted");
-            /**
-             * Implement this one day
-             *
-             */
+        if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
+            Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
+
+            int state = intent.getIntExtra("state", -1);
+            switch (state) {
+                case 0:
+                    Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Headset is unplugged");
+                    break;
+                case 1:
+                    Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Headset is plugged");
+                    break;
+                default:
+                    Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "I have no idea what the headset state is");
+            }
         }
     }
+
+
 }
