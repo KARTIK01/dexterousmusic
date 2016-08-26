31397ae24c45c9c899ac2cbb03c10a805119a657
kartik agarwal <kartik@zuppit.in>
2016-08-26 09:14:39 +0530

    changes

--- app/src/main/java/music/dexterous/com/dexterousmusic/receiver/HeadsetBroadcastReceiver.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/receiver/HeadsetBroadcastReceiver.java
@@ -3,10 +3,16 @@ package music.dexterous.com.dexterousmusic.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
 
-//TODO
+import music.dexterous.com.dexterousmusic.customeviews.ShortToast;
+import music.dexterous.com.dexterousmusic.utils.logger.PrettyLogger;
+
+//TODO register it with service it cannot br registed with manifest
+
+/**
+ * refer https://developer.android.com/reference/android/media/AudioManager.html#ACTION_HEADSET_PLUG
+ */
 public class HeadsetBroadcastReceiver extends BroadcastReceiver {
     String TAG = "Receiver";
 
@@ -15,22 +21,23 @@ public class HeadsetBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
-            Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
+        if (intent != null && intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
+            ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
 
             int state = intent.getIntExtra("state", -1);
+            int microPhone = intent.getIntExtra("microphone", -1); // 1 when has microphone else 0
             switch (state) {
                 case 0:
-                    Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
-                    Log.e(TAG, "Headset is unplugged");
+                    ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+                    PrettyLogger.d("Headset is unplugged");
                     break;
                 case 1:
-                    Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
-                    Log.e(TAG, "Headset is plugged");
+                    ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+                    PrettyLogger.d("Headset is plugged");
                     break;
                 default:
-                    Toast.makeText(context, "Received", Toast.LENGTH_SHORT).show();
-                    Log.e(TAG, "I have no idea what the headset state is");
+                    ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+                    PrettyLogger.d("I have no idea what the headset state is");
             }
         }
     }
