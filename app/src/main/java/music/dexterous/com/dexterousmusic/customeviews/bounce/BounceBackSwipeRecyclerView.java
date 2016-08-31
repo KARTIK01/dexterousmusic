e376f238a6a46bbd6957d61b156c513253a5d3a2
kartikagarwal01@gmail.com <Kartik_96540130>
2016-08-31 22:35:12 +0530

    changes

--- app/src/main/java/music/dexterous/com/dexterousmusic/customeviews/bounce/BounceBackSwipeRecyclerView.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/customeviews/bounce/BounceBackSwipeRecyclerView.java
@@ -3,6 +3,7 @@ package music.dexterous.com.dexterousmusic.customeviews.bounce;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.AttributeSet;
 
 import music.dexterous.com.dexterousmusic.customeviews.bounce.helper.OverScrollDecoratorHelper;
@@ -22,13 +23,31 @@ public class BounceBackSwipeRecyclerView extends RecyclerView {
 
     public BounceBackSwipeRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-         
+
     }
 
     @Override
     public void setAdapter(Adapter adapter) {
         super.setAdapter(adapter);
         OverScrollDecoratorHelper.setUpOverScroll(this, OverScrollDecoratorHelper.ORIENTATION_VERTICAL);
+
+        // Set-up of recycler-view's native item swiping.
+        ItemTouchHelper.Callback itemTouchHelperCallback = new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                return makeMovementFlags(0, ItemTouchHelper.RIGHT);
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int direction) {
+                //TODO delete song from playlist
+            }
+        };
     }
-    
+
 }
