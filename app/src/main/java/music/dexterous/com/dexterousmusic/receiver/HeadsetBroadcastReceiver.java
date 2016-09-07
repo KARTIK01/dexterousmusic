6244e682687fee4eb305a3669e5a2fedc884c125
kartikagarwal01@gmail.com <Kartik_96540130>
2016-09-07 23:39:24 +0530

    headset reaciver

--- app/src/main/java/music/dexterous/com/dexterousmusic/receiver/HeadsetBroadcastReceiver.java
+++ app/src/main/java/music/dexterous/com/dexterousmusic/receiver/HeadsetBroadcastReceiver.java
@@ -3,6 +3,7 @@ package music.dexterous.com.dexterousmusic.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.view.KeyEvent;
 import android.widget.Toast;
 
 import music.dexterous.com.dexterousmusic.customeviews.widget.ShortToast;
@@ -13,6 +14,18 @@ import music.dexterous.com.dexterousmusic.utils.logger.PrettyLogger;
 /**
  * refer https://developer.android.com/reference/android/media/AudioManager.html#ACTION_HEADSET_PLUG
  */
+
+/**
+ * Receives external Broadcasts and gives our MusicService orders based on them.
+ * <p>
+ * It is the bridge between our application and the external world. It receives Broadcasts and
+ * launches Internal Broadcasts.
+ * <p>
+ * It acts on music events (such as disconnecting headphone) and music controls (the lockscreen
+ * widget).
+ *
+ * @note This class works because we are declaring it in a `receiver` tag in `AndroidManifest.xml`.
+ */
 public class HeadsetBroadcastReceiver extends BroadcastReceiver {
     String TAG = "Receiver";
 
@@ -21,26 +34,75 @@ public class HeadsetBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent != null && intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
-            ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+        if (intent == null)
+            return;
 
-            int state      = intent.getIntExtra("state", -1);
-            int microPhone = intent.getIntExtra("microphone", -1); // 1 when has microphone else 0
-            switch (state) {
-                case 0:
-                    ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
-                    PrettyLogger.d("Headset is unplugged");
-                    break;
-                case 1:
-                    ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
-                    PrettyLogger.d("Headset is plugged");
-                    break;
-                default:
-                    ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
-                    PrettyLogger.d("I have no idea what the headset state is");
-            }
+        String action = intent.getAction();
+
+        switch (action) {
+            case Intent.ACTION_HEADSET_PLUG:
+                handleHeadSet(intent, context);
+                break;
+            case Intent.ACTION_MEDIA_BUTTON:
+                // Which media key was pressed
+                KeyEvent keyEvent = (KeyEvent) intent.getExtras().get(Intent.EXTRA_KEY_EVENT);
+                handleKeyPressed(keyEvent);
+                break;
+            case android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY:
+                break;
         }
     }
 
+    private void handleHeadSet(Intent intent, Context context) {
+        int state      = intent.getIntExtra("state", -1);
+        int microPhone = intent.getIntExtra("microphone", -1); // 1 when has microphone else 0
+        switch (state) {
+            case 0:
+                ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+                PrettyLogger.d("Headset is unplugged");
+                break;
+            case 1:
+                ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+                PrettyLogger.d("Headset is plugged");
+                break;
+            default:
+                ShortToast.displayToast(context, "Received", Toast.LENGTH_SHORT);
+                PrettyLogger.d("I have no idea what the headset state is");
+        }
+    }
 
+    private void handleKeyPressed(KeyEvent keyEvent) {
+
+        // Not interested on anything other than pressed keys.
+        if (keyEvent.getAction() != KeyEvent.ACTION_DOWN)
+            return;
+
+        switch (keyEvent.getKeyCode()) {
+
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+            case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+//                intentValue = ServicePlayMusic.BROADCAST_ORDER_TOGGLE_PLAYBACK;
+                break;
+
+            case KeyEvent.KEYCODE_MEDIA_PLAY:
+//                intentValue = ServicePlayMusic.BROADCAST_ORDER_PLAY;
+                break;
+
+            case KeyEvent.KEYCODE_MEDIA_PAUSE:
+//                intentValue = ServicePlayMusic.BROADCAST_ORDER_PAUSE;
+                break;
+
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+//                intentValue = ServicePlayMusic.BROADCAST_ORDER_SKIP;
+                break;
+
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                // TODO: ensure that doing this in rapid succession actually plays the
+                // previous song
+//                intentValue = ServicePlayMusic.BROADCAST_ORDER_REWIND;
+                break;
+        }
+    }
 }
+
+
